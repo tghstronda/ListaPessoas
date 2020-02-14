9b0ea5bf6f8d12dbc55baf5ae08f4a16d1941842
Tghstronda <jamedbonde31@gmail.com>
2020-02-13 20:38:23 -0400

    Titulo e Layout + reaproveitando component

--- App.js
+++ App.js
@@ -8,21 +8,8 @@ export default class App extends React.Component {
   render(){ 
     return (
       <View>
-        <Header />
-        <Header />
-        <Header />
-        <Header />
+        <Header title = "Lista de Pessoas"/>
       </View>
     );
   }
 }
-
-/*const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-*/
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { apisAreAvailable } from 'expo';
+import {KeyboardAvoidingView} from 'react-native';
 
 const Header = (props) => (
 
-        <View>
-            <Text>Esse e nosso Header</Text>
+        <View style = {style.container} behavior="padding" enabled>
+            <Text style = {style.title} behavior="padding" enabled>
+                {props.title}
+            </Text>
         </View>
 );
+
+const style = StyleSheet.create({
+    Header:{
+        backgroundColor:'#008b8b',
+    },
+    container: {
+      marginTop: 30,
+      //flex: 1,
+      backgroundColor: '#008b8b',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 25,
+    },
+    title:{
+        fontSize: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        color:"#fff",
+    }
+  });
+
 
 export default Header;
