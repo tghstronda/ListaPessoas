3ced8d8075affe340652d82a297937e0b2330c83
Tghstronda <jamedbonde31@gmail.com>
2020-02-17 20:20:37 -0400

    commit de lista simbolica teste &&  Instalação do Axios

--- App.js
+++ App.js
@@ -4,12 +4,30 @@ import { StyleSheet, Text, View} from 'react-native';
 import Header from './src/components/Header';
 
 export default class App extends React.Component {
- 
+  renderList(){
+      const names = 
+      [
+        'James Rosa de Lima',
+        'Rodrigo Santoro',
+        'Daniela Mercuri',
+        'Pericles de Souza',
+      ];
+
+      const textElements = names.map(name => {
+        return <Text key = {name}>{name}</Text>
+      });
+      return textElements;
+  }
+
   render(){ 
     return (
       <View>
         <Header title = "Lista de Pessoas"/>
+        {this.renderList()}
       </View>
     );
   }
+  /**Há diversos momentos quando você está desenvolvendo uma aplicação Web 
+   * que podem necessitar consumir e exibir dados de uma API.
+   *  Há várias maneiras de se fazer isso, mas a maneira mais popular é usando axios */
 }
